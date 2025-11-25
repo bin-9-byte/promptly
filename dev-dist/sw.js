cea9d7565cc53aae479bcb0c297b7d146cb6fded
mabin <mabin20231996@gmail.com>
2025-11-25 10:51:22 +0800

    feat: Integrate Tailwind CSS and PostCSS for styling, externalize CSS, and add a service worker.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7v4q61404vg"
+    "revision": "0.a6h28gm0uro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
